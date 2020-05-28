b04a47254a558b0ee7fcc8f71ab3162b7b6511fd
balajireddylachhannagari <devopstrainingbanglore@gmail.com>
2020-05-28 22:26:38 +0530

    Updated

--- VPC/instances.tf
+++ VPC/instances.tf
@@ -1,11 +1,11 @@
 # Demo Ubuntu Server(t2.medium)
 resource "aws_instance" "Demo_Server" {
-  count           = 1
-  ami             = var.ubuntuami
-  instance_type   = "t2.medium"
-  security_groups = [aws_security_group.demo_sg.id]
-  subnet_id       = element(aws_subnet.subnets.*.id, count.index)
-  key_name        = var.key_name
+  count                  = 1
+  ami                    = var.ubuntuami
+  instance_type          = "t2.medium"
+  vpc_security_group_ids = [aws_security_group.demo_sg.id]
+  subnet_id              = element(aws_subnet.subnets.*.id, count.index)
+  key_name               = var.key_name
 
   tags = {
     Name = "UbuntuServer"
@@ -15,12 +15,12 @@ resource "aws_instance" "Demo_Server" {
 }
 # 2 Instance Of Redhat Servers(t2.micro)
 resource "aws_instance" "redhat_servers" {
-  count           = 2
-  ami             = var.redhatami
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.demo_sg.id]
-  subnet_id       = element(aws_subnet.subnets.*.id, count.index)
-  key_name        = var.key_name
+  count                  = 2
+  ami                    = var.redhatami
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.demo_sg.id]
+  subnet_id              = element(aws_subnet.subnets.*.id, count.index)
+  key_name               = var.key_name
 
   tags = {
     Name = "Redhat_Server_${count.index + 1}"
